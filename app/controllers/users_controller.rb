416b6d313d9405e9f7bd0bab8f2f660ef21c449e
sagarpatel8384 <sagarpatel8384@gmail.com>
2016-03-25 16:42:39 -0400

    Add Bootstrap and Remove Turbolinks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to :show
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :password, :password_confirmation,
+      :status, :github_url, :linked_in_url
+    )
   end
 end
